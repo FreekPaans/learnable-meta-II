8de33d64d5b1251313cd40a4cf25fe18d9814b94
Freek Paans <freekpaans@gmail.com>
2015-07-19 15:41:26 +0200

    vm

---
+++ meta-II-vm.js
@@ -0,0 +1,23 @@
+var vm = require("./meta-II-vm-compiler")
+var fs = require("fs")
+
+var sourceFile = process.argv[2];
+var compilerFile = process.argv[3];
+
+console.warn("compiling %s with %s", sourceFile, compilerFile)
+
+fs.readFile(sourceFile, "utf8", function(err, source) {
+	if(err) {
+		console.error("Failed reading file " + err)
+		process.exit(1)
+	}
+
+	fs.readFile(compilerFile, function(err, compiler) {
+		if(err) {
+			console.error("Failed reading compiler file " + err)
+			process.exit(1)
+		}
+
+		console.log(vm(source,compiler))
+	});
+});
